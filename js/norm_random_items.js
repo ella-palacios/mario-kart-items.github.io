a9c3b8fb61b6249a869061f7d801d413b989835e
Ella Palacios <163610121+ella-palacios@users.noreply.github.com>
2024-10-21 13:17:06 -0700

    bug fixes norm_random_items.js

--- js/norm_random_items.js
+++ js/norm_random_items.js
@@ -1,16 +1,24 @@
 const NormalItems = ['banana', 'triple bananas', 'green shell', 'triple green shells', 'red shell', 'triple red shells', 'blue shell', 
                       'bob-omb', 'mushroom', 'triple mushrooms', 'golden mushroom', 'bullet', 'squid', 'lightning', 'star', 'fire flower', 
                       'boomerang flower', 'pirahna plant', 'sonic speaker', 'super eight', 'coin', 'king boo'];
-function NormalRandomItems(){
+
+function normRandomItems() {
     const normItemSize = NormalItems.length;
-    const numNormItems = Math.floor(Math.random() * (normItemSize +1); 
+    const numNormItems = Math.floor(Math.random() * (normItemSize + 1)); 
+
+    
     const itemsCopy = [...NormalItems];
 
+    // Select random items without replacement
     const itemsUsed = [];
-    for (let i = 0; i < numNormItems; i++){
-      const randomIndex = Math.floor(Math.random() * itemsCopy.length);
-      itemsUsed.push(itemsCopy[randomIndex]);
-      itemsCopy.splice(randomIndex, 1);
+    for (let i = 0; i < numNormItems; i++) {
+        const randomIndex = Math.floor(Math.random() * itemsCopy.length);
+        itemsUsed.push(itemsCopy[randomIndex]);
+        itemsCopy.splice(randomIndex, 1); // Remove the selected item from the copy
     }
-  return itemsUsed;
+
+    return itemsUsed;
 }
+
+//usage
+//console.log(normRandomItems());
